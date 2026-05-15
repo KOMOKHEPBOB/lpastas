d20db1702373ffa48f9821703e9bbc44fcfd1df3
JaN <naruskevic.jan@gmail.com>
2026-05-15 19:54:39 +0300

    Add missing strict types

--- src/Controller/Order/CreateOrderController.php
+++ src/Controller/Order/CreateOrderController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller\Order;
 
 use App\DTO\CreateOrderRequest;
@@ -19,8 +21,7 @@ class CreateOrderController extends AbstractController
 
     public function __invoke(
         #[MapRequestPayload] CreateOrderRequest $createOrderRequest,
-    ): JsonResponse
-    {
+    ): JsonResponse {
         $order = $this->orderCreator->createOrder($createOrderRequest);
 
         return new JsonResponse(['success' => true, 'message' => 'Created']);
